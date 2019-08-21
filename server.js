e0c0eb0613acc41fd6b3c5b98f198a31c7f69a16
Kelsy <kelsywells@gmail.com>
2019-08-20 19:25:54 -0700

    Added CRUD endpoints in server.js

--- server.js
+++ server.js
@@ -7,3 +7,70 @@ const server = express();
 server.use(express.json());
 
 module.exports = server;
+
+
+server.get('/', (req, res) => {
+    db('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts);
+    }
+    )
+    .catch(err => {
+        res.status(500).json({
+            message: 'Could not find accounts'
+        })
+    })
+})
+
+server.post('/', (req, res) => {
+    const account= req.body;
+    const name= req.body;
+    const budget= req.body;
+
+    db('accounts').insert(account)
+    .then(post => {
+        if (!name || !budget)
+        res.status(400).json({ message: 'Please include name and budget'})
+        else {
+            res.status(201).json({ message: 'Account added successfully'})
+        }
+    })
+    .catch(err=> {
+        res.status(500).json({
+            message: 'Could not add account.'
+        })
+    })
+})
+
+server.put('/:id', (req, res) => {
+    const changes= req.body;
+    const name= req.body;
+    const budget= req.body;
+    
+    db('accounts').where(name || budget).update(changes)
+    .then(edits => {
+        res.status(200).json(edits)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Could not update account.'
+        })
+    })
+})
+
+server.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db('accounts').where({ id }).del()
+    .then(deleted => {
+        res.status(200).json({ 
+            message: 'Account removed.'
+        })
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Could not remove account.'
+        })
+    })
+})
+
